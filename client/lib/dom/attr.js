31b0cfe149c7f9342384919425e028041a098bf5
jimyeong21 <jimyeong21@gmail.com>
2024-11-15 09:40:52 +0900

    setAttr 함수 추가

--- client/lib/dom/attr.js
+++ client/lib/dom/attr.js
@@ -3,3 +3,20 @@ function getAttr(node, prop) {
   if (!isString(prop)) throw typeError('getAttr 함수에 전달된 두번째 인수는 문자 타입 이어야 합니다.');
   return node.getAttribute(prop);
 }
+
+function setAttr(node, prop, value) {
+  if (isString(node)) node = getNode(node);
+  if (!isString(prop)) throw typeError('setAttr 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
+  if (isUndefined(value) || value === '') {
+    node.removeAttribute(prop);
+    return;
+  }
+
+  if (prop.startsWith('data')) {
+    prop = prop.slice(5);
+    node.dataset[prop] = value;
+    return;
+  }
+
+  node.setAttribute(prop, value);
+}
