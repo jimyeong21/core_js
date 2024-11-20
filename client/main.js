f3e7dcdf091b57cdbb84bacce6e8bd29ac3ed318
jimyeong21 <jimyeong21@gmail.com>
2024-11-20 09:36:24 +0900

    phase2 추가

--- client/main.js
+++ client/main.js
@@ -68,6 +68,26 @@ function phase1() {
 
 function phase2() {
   const calculator = $('.calculator');
+  const result = $('.result');
+  const clear = $('#clear');
+  const numberInputs = [...document.querySelectorAll('input:not(#clear)')];
+
+  function handleInput() {
+    const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
+    console.log(total);
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+    numberInputs.forEach(clearContents);
+    result.textContent = '-';
+  }
+
+  calculator.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
 }
 
 phase2();
