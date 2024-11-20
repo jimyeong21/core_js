6a72c9ff52712b0c65c94f0dd2d9f4b080597286
jimyeong21 <jimyeong21@gmail.com>
2024-11-20 09:22:15 +0900

    모듈형 프로그래밍, phase1 함수 추가

--- client/lib/error/typeError.js
+++ client/lib/error/typeError.js
@@ -1,3 +1,5 @@
-function typeError(message) {
-  return new typeError(message);
+
+
+export function typeError(message){
+  return new TypeError(message)
 }
---
+++ client/main.js
@@ -0,0 +1,73 @@
+// named export           =>  import { } from '..'
+// default export         =>  import ... from '..'
+
+// import { getNode as $, getNodes } from './lib/dom/getNode.js';
+// import { insertLast } from './lib/dom/insert.js';
+
+// import clearContents from "./lib/dom/clearContents.js";
+
+import { getNode as $, getNodes, typeError, insertLast, clearContents } from './lib/index.js';
+
+// 1. input 선택하기
+// 2. input 이벤트 바인딩
+// 3. input의 value 값 가져오기
+// 4. 숫자 더하기
+// 5. result에 출력하기
+
+const first = $('#firstNumber');
+const second = $('#secondNumber');
+const result = $('.result');
+const clear = $('#clear');
+
+function handleInput() {
+  const firstValue = Number(first.value);
+  const secondValue = +second.value;
+  const total = firstValue + secondValue;
+
+  clearContents(result);
+  insertLast(result, total);
+}
+
+function handleClear(e) {
+  e.preventDefault();
+  clearContents(first);
+  clearContents(second);
+  result.textContent = '-';
+}
+
+first.addEventListener('input', handleInput);
+second.addEventListener('input', handleInput);
+clear.addEventListener('click', handleClear);
+
+function phase1() {
+  const first = $('#firstNumber');
+  const second = $('#secondNumber');
+  const result = $('.result');
+  const clear = $('#clear');
+
+  function handleInput() {
+    const firstValue = Number(first.value);
+    const secondValue = +second.value;
+    const total = firstValue + secondValue;
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+    clearContents(first);
+    clearContents(second);
+    result.textContent = '-';
+  }
+
+  first.addEventListener('input', handleInput);
+  second.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+
+function phase2() {
+  const calculator = $('.calculator');
+}
+
+phase2();
